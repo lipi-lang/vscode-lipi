df4c6f100894ba04b064b2932a0e4c090c4c5b50
Ta-Seen Islam <tahscenery@gmail.com>
2022-02-26 23:40:17 +1100

    Add basic syntax highlighting support

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,3 @@
 import * as vs from 'vscode';
 
-export function activate(context: vs.ExtensionContext) {
-  vs.window.showInformationMessage('Welcome to Lipi!');
-}
+export function activate(context: vs.ExtensionContext) {}
